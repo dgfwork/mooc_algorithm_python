2725881fc735606703e96d3d8d737beba65215c0
scavenger <dgfwork@163.com>
2019-05-01 21:59:35 +0800

    DS

--- DS/bubblesort.py
+++ DS/bubblesort.py
@@ -10,10 +10,15 @@ def bubblesort(nums):
     n = len(nums)
     for i in range(n - 1):
         # 第一层表示需要排这么多趟
+        change_flag = False
         for j in range(n - 1 - i):
             # 第二层里面的长度是动态的
             if nums[j] > nums[j + 1]:
+                change_flag = True
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        # 优化一下有序的情况
+        if not change_flag:
+            break
     return nums
 
 
