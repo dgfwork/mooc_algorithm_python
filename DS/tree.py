a38a66b34f93e3f20ce4e704d0b201b496d0727c
scavenger <dgfwork@163.com>
2019-05-01 19:56:48 +0800

    DS

--- DS/tree.py
+++ DS/tree.py
@@ -20,7 +20,8 @@ class Tree(object):
     def add(self, elem):
         node = Node(elem)
         if self.root is None:
-            self.root = Node
+            self.root = node
+            return
         queue = [self.root]
         while queue:
             cur_node = queue.pop(0)
@@ -29,12 +30,32 @@ class Tree(object):
                 return
             else:
                 queue.append(cur_node.lchild)
+
             if cur_node.rchild is None:
-                cur_node.lchild = node
+                cur_node.rchild = node
                 return
             else:
-                queue.append(cur_node.lchild)
+                queue.append(cur_node.rchild)
+
+    def breadth_travel(self):
+        if self.root is None:
+            return
+        else:
+            queue = [self.root]
+            while queue:
+                cur_node = queue.pop(0)
+                print(cur_node.elem)
+                if cur_node.lchild:
+                    queue.append(cur_node.lchild)
+                if cur_node.rchild:
+                    queue.append(cur_node.rchild)
 
 
 if __name__ == "__main__":
     tree = Tree()
+    tree.add(20)
+    tree.add(10)
+    tree.add(80)
+    tree.add(100)
+    tree.add(178)
+    tree.breadth_travel()
