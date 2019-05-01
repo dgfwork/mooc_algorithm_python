4805dbfc97d1abdb4d4d22caf32767fc4e9453d7
scavenger <dgfwork@163.com>
2019-05-01 19:30:57 +0800

    09-Dynamic-Programming

---
+++ DS/tree.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/1 19:16
+# @Author  : Scavenger
+# @FileName: tree.py
+# @Software: PyCharm
+# @Github  : https://github.com/dgfwork/
+# @Blog    : https://www.cnblogs.com/gaofeng-d/
+
+class Node(object):
+    def __init__(self, elem):
+        self.elem = elem
+        self.rchild = None
+        self.lchild = None
+
+
+class Tree(object):
+    def __init__(self):
+        self.root = None
+
+    def add(self, elem):
+        node = Node(elem)
+        if self.root is None:
+            self.root = Node
+        queue = [self.root]
+        while queue:
+            cur_node = queue.pop(0)
+            if cur_node.lchild is None:
+                cur_node.lchild = node
+                return
+            else:
+                queue.append(cur_node.lchild)
+            if cur_node.rchild is None:
+                cur_node.lchild = node
+                return
+            else:
+                queue.append(cur_node.lchild)
