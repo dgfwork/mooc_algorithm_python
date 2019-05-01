f3ea5ae4454045fe680a92db05283d9dcc6de530
scavenger <dgfwork@163.com>
2019-05-01 21:50:25 +0800

    DS

---
+++ DS/bubblesort.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/1 21:24
+# @Author  : Scavenger
+# @FileName: bubblesort.py
+# @Software: PyCharm
+# @Github  : https://github.com/dgfwork/
+# @Blog    : https://www.cnblogs.com/gaofeng-d/
+
+def bubblesort(nums):
+    n = len(nums)
+    for i in range(n - 1):
+        # 第一层表示需要排这么多趟
+        for j in range(n - 1 - i):
+            # 第二层里面的长度是动态的
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+
+if __name__ == "__main__":
+    lst = bubblesort([3, 2, 4, 1, 8, 7])
+    print(lst)
